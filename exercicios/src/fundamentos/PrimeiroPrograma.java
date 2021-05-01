09b32c6b26442cfb64190eeed785ac807b790c82
Edilson Alves <edilsonsck@outlook.com>
2021-05-01 14:17:15 -0300

    exercicioComentarios
    apenas quando for relevante(LEMBRE-SE!!)

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,11 +1,31 @@
 package fundamentos;
 
 
-
+/**
+ * Essa classe representa... JAVADOC
+ * 
+ * @author Edilson <@email.com>
+ * @sinse 1.0
+ * @see
+ */
 public class PrimeiroPrograma 
 {
+	// APENAS UTILIZE COMENTARIOS QUANDO FOR RELEVANTE!!!
+	/**
+	 * 
+	 * @param args
+	 * 
+	 */
 	public static void main(String[] args) 
 	{
+		// Um sentença de código termina com ; (ponto e virgula)
+		// Mais um comentário...
+		// Fim
 		System.out.println("Hello World!");
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3...
+		 */
 	}
 }
