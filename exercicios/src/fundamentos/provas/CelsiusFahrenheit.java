5c3446dc3775dd60c82cd33529022093bc010081
Edilson Alves <edilsonsck@outlook.com>
2021-05-03 22:59:25 -0300

    Prova1CelsiusFahrenheit

---
+++ exercicios/src/fundamentos/provas/CelsiusFahrenheit.java
@@ -0,0 +1,19 @@
+package fundamentos.provas;
+
+import java.util.Scanner;
+
+public class CelsiusFahrenheit 
+{
+	public static void main(String[] args) 
+	{
+		//Formula (C x 9/5) + 32
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite o graus em Celsius : ");
+		double celsius = entrada.nextDouble();
+		double num1 = 9/5;
+		double num2 = 32;
+		double fahrenheit = (celsius *  num1) + num2;
+		System.out.printf("A conversao em Fahrenheit é : %.2f", fahrenheit);
+		entrada.close();		
+	}
+}
