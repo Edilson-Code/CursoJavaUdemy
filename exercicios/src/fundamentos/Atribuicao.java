8ce2b9bd76fbb64b9d0ee44de6f2971e010b8232
Edilson Alves <edilsonsck@outlook.com>
2021-05-03 21:20:03 -0300

    exercicioAtribuicao

---
+++ exercicios/src/fundamentos/Atribuicao.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class Atribuicao 
+{
+	public static void main(String[] args) 
+	{
+		int a = 3;
+		int b = a;
+		int c = a + b;
+		
+		c += b;//c = c + b;
+		c -= a;//c = c - a;
+		c *= b;//c = c * b;
+		c /= a;//c = c / a;
+		c %= 2;//c = c % c; 0 ou 1
+		
+	}
+}
