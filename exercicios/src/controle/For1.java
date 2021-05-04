386dd9c97578560d81b24daba8664943aa37cea6
Edilson Alves <edilsonsck@outlook.com>
2021-05-04 19:56:32 -0300

    exercicioFor1

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,24 @@
+package controle;
+
+public class For1 
+{
+	public static void main(String[] args) 
+	{
+		for(int i = 0; i <= 3; i ++)
+		{
+			System.out.println("Hello world!");
+		}
+		
+		int x = 2;
+		for(; x<10;) 
+		{
+			System.out.println("x = " + x);
+			x++;
+		}
+		//laço infinito!!!
+//		for(;;)
+//		{
+//			System.out.println("Fim");
+//		}
+	}
+}
