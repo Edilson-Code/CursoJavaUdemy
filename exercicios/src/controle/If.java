8f488af21da6b5612033874b38cf8b27fddcd643
Edilson Alves <edilsonsck@outlook.com>
2021-05-04 19:11:41 -0300

    exercicioIf

---
+++ exercicios/src/controle/If.java
@@ -0,0 +1,31 @@
+package controle;
+
+import java.util.Scanner;
+
+public class If 
+{
+	public static void main(String[] args) 
+	{
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Inform a média : ");
+		double media = entrada.nextDouble();
+		if(media <= 10 && media >= 7)
+		{
+			System.out.println("Aprovado!");
+		}
+		if(media <= 7 && media >= 4.5)
+		{
+			System.out.println("Recuperação");
+		}
+		
+		boolean criterioReprovacaoAtingido = media < 4.5 && media >= 0;
+		
+		if(criterioReprovacaoAtingido)
+		{
+			System.out.println("Reprovado");
+		}
+		
+		entrada.close();
+	}
+}
