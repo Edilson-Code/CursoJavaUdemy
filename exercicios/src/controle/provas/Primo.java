fae5c2aff6201fef2937bbf7859ccf42265d0f7b
Edilson Alves <edilsonsck@outlook.com>
2021-05-04 22:55:11 -0300

    provasPrimo

---
+++ exercicios/src/controle/provas/Primo.java
@@ -0,0 +1,32 @@
+package controle.provas;
+
+import java.util.Scanner;
+
+public class Primo 
+{
+	public static void main(String[] args) 
+	{
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite um numero : ");
+		double num = entrada.nextDouble();
+		int contador = 0;
+		double primo = 0;
+		
+		while(contador <= num)
+		{
+			if(num % contador == 0)
+			{
+				primo++;
+			}
+			contador ++;			
+		}
+		if(primo <= 2)
+		{
+			System.out.println("O numero digitado é PRIMO");
+		}else
+		{
+			System.out.println("O numero digitado NÃO é PRIMO");
+		}
+		entrada.close();
+	}
+}
